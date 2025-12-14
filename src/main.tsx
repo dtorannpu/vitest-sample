1fab69b6d843512d71af45a9334fe85ab66341bb
dtorannpu <dtorannpu@gmail.com>
2025-12-14 10:36:10 +0900

    fix: remove file extensions from imports in main.tsx for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Top from "@/pages/Top.tsx";
-import Page1 from "@/pages/Page1.tsx";
-import Page2 from "@/pages/Page2.tsx";
+import Top from "@/pages/Top";
+import Page1 from "@/pages/Page1";
+import Page2 from "@/pages/Page2";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
