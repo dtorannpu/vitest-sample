e3a22ec168741db112d4d04f88305d0c77e93714
dtorannpu <dtorannpu@gmail.com>
2026-04-05 11:10:02 +0900

    style: format spacing in vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  resolve:{
+  resolve: {
     tsconfigPaths: true,
   },
-  
+
   test: {
     globals: true,
     environment: "jsdom",
